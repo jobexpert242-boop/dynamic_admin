d8a86d238efffc39c97e90a3a8621a864ffce8a8
Rajib <jobexpert242@gmail.com>
2026-01-01 11:57:42 +0600

    web add storage link

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,18 @@ use App\Http\Controllers\Admin\LanguageController;
 use App\Http\Controllers\Admin\PageController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 
 // all route here 
 Route::get('/', [UserController::class, 'home'])->name('home');
 
+
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+    return 'Storage link created';
+});
 
 // include route
 require __DIR__ . '/admin.php';
